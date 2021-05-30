e9b7ea998e10ccf802fbabbf826e7ed587140fb0
angelo-filho <amslf.snf20@uea.edu.br>
2021-05-30 17:46:52 -0400

    #Update: turning logic of main_menu.py  into a function

--- main_menu.py
+++ main_menu.py
@@ -1,40 +1,39 @@
-import pygame 
 from pygame.locals import *
 from constants import *
-from suports import color_variation, draw_text
+from suports import *
 
-# setting basic stats for menu window
-pygame.init()
-screen = pygame.display.set_mode((WINDOW, WINDOW))
-fps = pygame.time.Clock()
 
-# setting texts for menu options
-menu_texts = [("PLAY", 307, 284, 48, WHITE, screen), ("CREDITS", 278, 347, 48, WHITE, screen),
-             ("EXIT", 306, 410, 48, WHITE, screen)]
+def main_menu(screen):
+    # setting basic stats for menu window
+    fps = pygame.time.Clock()
 
-# value for text current orientation
-current_option = 0
+    # setting texts for menu options
+    menu_texts = [("PLAY", 307, 284, 48, WHITE, screen), ("CREDITS", 278, 347, 48, WHITE, screen),
+                  ("EXIT", 306, 410, 48, WHITE, screen)]
 
-# verification for loop
-is_running = True
+    # value for text current orientation
+    current_option = 0
 
-# loading the instructions images
-instructions_left = pygame.image.load("assets/instructions_left.png")
-instructions_right = pygame.image.load("assets/instructions_right.png")
+    # verification for loop
+    is_running = True
 
-# setting initial color for rgb
-menu_color = pygame.Color("#E16D2B")
-options_color = pygame.Color(RED)
+    # loading the instructions images
+    instructions_left = pygame.image.load("assets/instructions_left.png")
+    instructions_right = pygame.image.load("assets/instructions_right.png")
 
-while is_running:
-        
+    # setting initial color for rgb
+    menu_color = pygame.Color("#E16D2B")
+    options_color = pygame.Color(RED)
+
+    while is_running:
+
         # frames per second for window menu
         fps.tick(60)
 
         # getting the events in menu window
         for event in pygame.event.get():
             if event.type == QUIT:
-                is_running = False
+                quit_game()
             elif event.type == KEYDOWN:
                 # getting action key
                 if event.key == K_UP:
@@ -48,12 +47,12 @@ while is_running:
                 # getting the select key
                 elif event.key == K_SPACE:
                     if current_option == 2:
-                        is_running = False
+                        quit_game()
 
         screen.fill(BLACK)
 
         # drawing the menu text
-        draw_text("MENU", 276, 105, 92, menu_color, screen)        
+        draw_text("MENU", 276, 105, 92, menu_color, screen)
         # function for rgb text
         color_variation(menu_color)
 
@@ -67,7 +66,5 @@ while is_running:
         # drawing the instructions
         screen.blit(instructions_left, (65, 630))
         screen.blit(instructions_right, (508, 630))
-
-
 
         pygame.display.flip()
