88e642ca7094bff38062f124e77f72d0586406bd
Roberto Antônio <74279517+robertoantony32@users.noreply.github.com>
2021-05-31 02:02:39 -0300

    #Feature: Adding a end game window

---
+++ end_game_screen.py
@@ -0,0 +1,45 @@
+from main_menu import main_menu
+from suports import *
+from pygame.locals import *
+from constants import *
+
+pygame.init()
+
+screen = pygame.display.set_mode((WINDOW, WINDOW))
+    
+is_running = True
+
+end_game_text = [("TRY AGAIN", 256, 451, 50, WHITE, screen), ("BACK TO MENU", 229, 516, 50, WHITE, screen)]
+current_option = 0
+options_color = pygame.Color(RED)
+
+    
+while is_running:
+    for event in pygame.event.get():
+        if event.type == QUIT: 
+            quit_game()
+        elif event.type == KEYDOWN:
+            # getting action key
+            if event.key == K_UP:
+                current_option -= 1
+                if current_option < 0:
+                    current_option = 1
+            elif event.key == K_DOWN:
+                current_option += 1
+                if current_option > 1:
+                    current_option = 0
+            elif event.key == K_SPACE:
+                if current_option == 0:
+                    pass
+                elif current_option == 1:
+                    main_menu(screen)
+            
+    
+    screen.fill(BLACK)
+
+    draw_text("GAME", 114, 162, 122, YELLOW, screen)
+    draw_text("OVER!!", 321, 164, 122, YELLOW, screen)
+
+    option_choice(end_game_text, current_option, screen, options_color)
+        
+    pygame.display.flip()
